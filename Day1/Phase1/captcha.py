a13e746378a527c6a04834103183e7b424fdf09f
Adrian Maliszewski <adrian@aftre.net>
2020-03-21 20:43:14 -0400

    2020 updates

---
+++ Day1/Phase1/captcha.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+fh = open("input.txt", mode='r')
+f_in = fh.read()
+fh.close()
+
+f_in = f_in.strip()
+
+nseq = []
+
+for i in f_in.split(' '):
+    nseq.append(int(i))
+
+mcnt = 0
+scnt = 0
+
+for i in range(len(nseq)):
+    scnt = scnt + 1
+
+    if nseq[i] < nseq[i-1]:
+        scnt = 1
+
+    if nseq[i] % 2 == 0 and nseq[i-1] % 2 == 0:
+        scnt = 1
+
+    if nseq[i] % 2 != 0 and nseq[i-1] % 2 != 0:
+        scnt = 1
+
+    if scnt > mcnt:
+        mcnt = scnt
+
+    print(nseq[i])
+
+print(mcnt)
